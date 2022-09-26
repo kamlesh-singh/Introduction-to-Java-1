6df7ea60ea16f127e28493f061ab2547caea596e
kamlesh <kamlesh.singh@tothenew.com>
2022-09-26 14:17:57 +0530

    Answer 5 : Create a class named Employee with fields firstname,lastname,age and designation.            The class should:
                   have all types of constructors to initialize the object
                   class should also have setter methods to update a particular field
                   Override its toString method to display a meaningful message using all these fields

---
+++ src/Q5.java
@@ -0,0 +1,54 @@
+public class Q5 {
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String designation;
+    Q5() {
+        this.firstname = "kamlesh";
+        this.lastname = "singh";
+        this.age = 25;
+        this.designation = "Software Engineer";
+    }
+    Q5(String firstname, String lastname, int age, String designation) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.designation = designation;
+    }
+    public void setFirstName(String firstName) {
+        this.firstname = firstName;
+    }
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+    @Override
+    public String toString() {
+        return "First Name : " + firstname + " Last Name: " + lastname + " Age : " + age + " Designation : " + designation;
+    }
+    public static void main(String[] args) {
+        Q5 obj1 = new Q5();
+        Q5 obj2 = new Q5("kamlesh", "singh", 25, "Software Engineer");
+
+        System.out.println("Initialized object using non-parameterised constructor");
+        System.out.println(obj1);
+
+        System.out.println("Initialized object using parameterised constructor");
+        System.out.println(obj2);
+
+    System.out.println("Updating instance variable of the object");
+    obj2.setFirstName("kamlesh2");
+    obj2.setLastName("Singh2");
+    obj2.setAge(25);
+    obj2.setDesignation("JVM SE");
+
+    System.out.println("Printing Updated value");
+    System.out.println(obj2);
+
+}
+}
