58250a004d9f800c68de07b209f7b5996403e6f7
kamlesh <kamlesh.singh@tothenew.com>
2022-09-26 14:21:21 +0530

    Answer 2: read user input until user writes XDONE and then show the entered text by the user on commandline

---
+++ src/Q2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Q2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String text="";
+       while(!((text=sc.nextLine()).equals("XDONE"))){
+           System.out.println(text);
+       }
+    }
+}
